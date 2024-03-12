2af2fe93f7dea32d0c59d745e2212d23141a2689
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-03-12 19:52:08 +0530

    Time: 1344 ms (5.23%) | Memory: 431.5 MB (5.10%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     vector<int> merge2SortedArrays(vector<int>&left,vector<int>& right){
+        int lftsz=left.size();
+        int rtsz=right.size();
         int i=0,j=0;
         vector<int> ans;
-        while(i<left.size() && j<right.size()){
+        while(i<lftsz && j<rtsz){
             if(left[i]<=right[j]){
-               ans.push_back(left[i]);
-               i++;
+                ans.push_back(left[i]);
+                i++;
             }
             else{
                 ans.push_back(right[j]);
                 j++;
             }
         }
-        while(i<left.size()){
-            ans.push_back(left[i++]);
+        while(i<lftsz){
+            ans.push_back(left[i]);
+            i++;
         }
-        while(j<right.size()){
-            ans.push_back(right[j++]);
+        while(j<rtsz){
+            ans.push_back(right[j]);
+            j++;
         }
         return ans;
+       
     }
     vector<int> sortArray(vector<int>& nums) {
         int n=nums.size();
         if(n==1){
             return nums;
         }
-      
         vector<int> left(nums.begin(),nums.begin()+n/2);
         vector<int> right(nums.begin()+n/2,nums.end());
-        
         left=sortArray(left);
         right=sortArray(right);
-
         return merge2SortedArrays(left,right);
-       
     }
 };
