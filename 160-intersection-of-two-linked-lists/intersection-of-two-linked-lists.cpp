08731686fe7dc6ff777ee6641ec0739a0f002aa4
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-03-13 22:22:41 +0530

    Time: 39 ms (59.88%) | Memory: 17 MB (64.66%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,36 +8,21 @@
  */
 class Solution {
 public:
-    int getLength(ListNode* node){
-        int len=0;
-        ListNode* cur=node;
-        while(cur!=NULL){
-            cur=cur->next;
-            len++;
-        }
-        return len;
-    }
-    ListNode* giveTheEdge(ListNode* head,int extra){
-        ListNode* cur=head;
-        for(int i=0;i<extra;i++){
-            cur=cur->next;
-        }
-        return cur;
-    }
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int len1=getLength(headA);
-        int len2=getLength(headB);
+       
         ListNode* curA=headA;
         ListNode* curB=headB;
-        if(len1 >len2){
-            curA=giveTheEdge( headA,len1-len2);
-        }
-        else{
-            curB=giveTheEdge( headB,len2-len1);
-        }
+        bool flag=false;
         while(curA !=curB){
             curA=curA->next;
+            if(curA==NULL){
+               curA=headB; 
+               if(flag)return NULL;
+               flag=true;
+            }
             curB=curB->next;
+            if(curB==NULL)curB=headA;
         }
         return curA;
     }
