9dc971be459b2b1f26ac5fc3eefd1647694fb5cc
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-03-14 22:52:53 +0530

    Time: 27 ms (70.37%) | Memory: 20.5 MB (34.90%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    //competitor function
-    bool static sorted(const vector<int>&v1,const vector<int>&v2){
-        if(v1[1]==v2[1]){
-            return v1[0]>=v2[0];
-        }
-        return v1[1]>v2[1];
+    static bool cmp(vector<int>&a,vector<int>&b){
+        return a[1]>b[1];
     }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(),boxTypes.end(),sorted);  
-        int j=0,n=0,ans=0;
-        cout<<j-1<<endl;
-        int total_boxes=0,total_weight=0;
-        for(int i=0;i<boxTypes.size();i++){
-           total_boxes+=boxTypes[i][0];
-           total_weight+=boxTypes[i][0]*boxTypes[i][1];
+        sort(boxTypes.begin(),boxTypes.end(),cmp);
+		 /*custom sort (in increasing order of 
+		 numberOfUnitsPerBox as we have to return  
+		 maximum total number of units )*/
+        int ans=0;
+        for(auto it:boxTypes){
+            int x=min(truckSize,it[0]);
+            ans+=it[1]*x; //adding units in ans
+            truckSize-=x; //reduce the capacity
+            if(truckSize==0){ // capacity is full
+                break;
+            }
         }
-        cout<<total_boxes<<endl;
-        cout<<total_weight<<endl;
-        while(truckSize>n && j<boxTypes.size()){
-            n+=boxTypes[j][0];
-            cout<<n<<" ";
-            ans+=boxTypes[j][0]*boxTypes[j][1];
-            cout<<ans<<" ";
-            j++;
-            cout<<j-1<<endl;
-        }
-        cout<<n<<" "<<ans<<endl;
-        if( n<=truckSize && total_boxes==n && ans<=total_weight )return ans;
-        ans+=(truckSize-n)*boxTypes[j-1][1];
-        cout<<ans<<" "<<j-1;
         return ans;
-        
-
     }
 };
