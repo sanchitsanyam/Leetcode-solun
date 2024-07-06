f1321c96810f987573eac379929b36340767a1ab
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-07-06 09:20:30 +0530

    Time: 197 ms (98.36%) | Memory: 92.3 MB (83.56%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        sort(items.begin(),items.end());
-        int n=queries.size();
-        int m=items.size();
-        vector<int>premax(m,0);
+        sort(items.begin(), items.end());
+        int n=items.size();
+        int m=queries.size();
+        vector<int> premax(n,0);
         premax[0]=items[0][1];
-        for(int i=1;i<m;i++){
-            premax[i]=max(premax[i-1],items[i][1]);
+        for(int i=1; i<n; i++){
+            premax[i]=max(items[i][1],premax[i-1]);
         }
-        vector<int> ans(n,0);
-        
-        for(int i=0;i<n;i++){
+        vector<int> ans(m,0);
+        for(int i=0;i<m;i++){
             int k=queries[i];
-            int l=0,r=m-1;
+            int l=0 ,r=n-1;
             int profit=-1;
             while(l<=r){
-                int mid=l+(r-l)/2;
-                if(items[mid][0]>k){
-                    r=mid-1;  
-                }
-                else{
+                int mid=(l+r)/2;
+                if(items[mid][0]<=k){
                     profit=mid;
                     l=mid+1;
                 }
-
+                else{
+                    r=mid-1;
+                }
             }
-            if(profit==-1)continue;
-            ans[i]=premax[profit];
-
+            if(profit!=-1){
+                ans[i]=premax[profit];
+            }
+           
         }
         return ans;
     }
