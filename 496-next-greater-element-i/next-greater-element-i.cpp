139c0a925e73183a91ebfb916233144baf42582f
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-07-21 17:15:20 +0530

    Time: 10 ms (12.80%) | Memory: 11.1 MB (80.77%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-       
-        stack<int> cat;
+        stack<int> st;
+        
         int n=nums2.size();
-        vector<int> ans(n,-1);
+        vector<int> nge(n,-1);
         for(int i=n-1;i>=0;i--){
-            if(cat.empty()){
-                ans[i]=-1;
-                cat.push(nums2[i]);
+            while(!st.empty() && st.top()<=nums2[i]){
+                st.pop();
             }
-            while((!cat.empty())&&(nums2[i]>=cat.top())){
-                cat.pop();
+            if(!st.empty()){
+               nge[i]=st.top();
             }
-            if(!cat.empty()){
-               ans[i]=cat.top();
-            }
-            cat.push(nums2[i]);
+            st.push(nums2[i]);
         }
         int n1=nums1.size();
-        vector<int> finalans(n1);
+        vector<int> ans(n1,-1);
         for(int i=0;i<n1;i++){
             for(int j=0;j<n;j++){
                 if(nums1[i]==nums2[j]){
-                    finalans[i]=ans[j];
+                    ans[i]=nge[j];
                 }
             }
         }
-        return finalans;
-
+        return ans;
     }
 };
