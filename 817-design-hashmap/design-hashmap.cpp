f828e02a3d2975b89f7419e6c70284d47f2aa40f
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-08-04 09:45:04 +0530

    Time: 103 ms (64.05%) | Memory: 57.3 MB (76.56%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -1,23 +1,56 @@
 class MyHashMap {
 public:
-    vector<int> m;
+    vector<list<pair<int,int>>> m;
     int siz ;
     MyHashMap() {
-       siz = 1e6+1;
-       m.resize(siz);
-       fill(m.begin() , m.end(),-1); 
+        siz=100;
+        m.resize(siz);
     }
-    
+    int Hash(int key){
+        return key%siz;
+    }
+    list<pair<int,int>>:: iterator Search(int key){
+        int i= Hash(key);
+        list<pair<int,int>>:: iterator it= m[i].begin();
+        while(it != m[i].end()){
+            if(it->first==key)return it;
+            it++;
+        }
+        return m[i].end();
+    }
     void put(int key, int value) {
-        m[key]=value;
+        int i =Hash(key);
+        if(contains(key)){
+            list<pair<int,int>>:: iterator it=Search(key);
+            it->second=value;
+        }
+        else{
+            m[i].push_back({key,value});
+        }
     }
     
     int get(int key) {
-        return m[key];
+        if(!contains(key))return-1;
+        else{
+            list<pair<int,int>>:: iterator it=Search(key);
+            return it->second;
+        }
+        
     }
     
     void remove(int key) {
-        m[key]=-1;
+        if(!contains(key))return;
+        else{
+            int i = Hash(key);
+            list<pair<int,int>>:: iterator it=Search(key);
+            m[i].erase(it);
+            
+        }
+    }
+    bool contains(int key){
+        int i=Hash(key);
+        if(Search(key)!=m[i].end())return true;
+        else return false;
     }
 };
 
