945a9a0d7dcab541c9c867a89e312905f9769545
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-08-15 11:04:56 +0530

    Time: 412 ms (60.45%) | Memory: 11.9 MB (42.25%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<bool> SeivePrime(int n){
-        vector<bool> ckp(n+1, true);
-        ckp[1]=false;
-        ckp[0]=false;
+   
+    vector<bool> seiveprime(int n){
+        vector<bool>p(n+1,true);
+        p[0]=false;
+        p[1]=false;
         for(int i=2;i*i<=n;i++){
-            if(ckp[i]==true && (long long)i*i<n){
-                for(int j=i*i;j<=n;j+=i){
-                    ckp[j]=false;
+            if(p[i]==true && (long long)i*i<n){
+                for(int j=i*i ; j<=n; j+=i){
+                    p[j]=false;
                 }
-            }    
-        }
-        return ckp;
-    }
-    int countPrimes(int n) {
-        
-        vector<bool> p=SeivePrime( n);
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(p[i]){
-                ans++;
             }
         }
-        
-        return ans;
+        return p;
+    }
+    int countPrimes(int n) {
+        vector<bool> p=seiveprime(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(p[i]){
+                cnt++;
+            }
+        }
+        return cnt;
     }
 };
