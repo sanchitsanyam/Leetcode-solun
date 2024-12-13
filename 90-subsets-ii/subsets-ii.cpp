02a8cc0b9cd5764fbc07c7bb98e6d6c8cddf529d
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2024-12-14 01:46:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (26.88%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     vector<vector<int>> ans;
     void help(int i ,vector<int> curr,vector<int>& arr ){
-        if(i==arr.size()){
-            ans.push_back(curr);
-            return;
+        if(i>arr.size())return;
+        ans.push_back(curr);
+        for(int j=i;j<arr.size();j++){
+            if(j!=i && arr[j]==arr[j-1]) continue;
+            curr.push_back(arr[j]);
+            help(j+1, curr,arr);
+            curr.pop_back();
         }
-        // for(int j=i;j<arr.size();j++){
-        //     //if(j!=i && arr[j]==arr[j-1]) continue;
-        //     curr.push_back(arr[j]);
-        //     help(i+1, curr,arr);
-        // }
-        curr.push_back(arr[i]);
-        help(i+1, curr,arr);
-        curr.pop_back();
-        help(i+1, curr,arr);
-
-        
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int> curr;
         help(0,curr,nums);
-        set<vector<int>> m(ans.begin(),ans.end());
-        vector<vector<int>> fans(m.begin(),m.end());
-        return fans;
+        return ans;
     }
 };
