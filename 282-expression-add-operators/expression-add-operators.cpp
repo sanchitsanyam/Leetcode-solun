2d1af49aab6f3478dd37a0fa8a2fdce0b5c80312
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-01-11 16:29:16 +0530

    Time: 426 ms (49.02%) | Memory: 94 MB (16.36%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    vector<string> ans; // To store valid expressions
-    int target;         // Target value
-
-    // Helper function for recursive exploration
-    void help(int i, long prev, long curr, string& num, string expr) {
-        // Base case: if we've processed all digits
-        if (i == num.size()) {
-            if (curr == target) {
-                ans.push_back(expr); // If the current value matches the target, add to result
+    vector<string> ans;
+    int target;
+    void help(int i, long prev , long curr , string& num ,string expr){
+        if(i==num.size()){
+            if(curr==target){
+                ans.push_back(expr);
             }
             return;
         }
-
-        for (int j = i; j < num.size(); j++) {
-            // Extract the current number as a substring
-            string part = num.substr(i, j - i + 1);
-            long val = stol(part);
-
-            // Skip numbers with leading zeros (except single '0')
-            if (part.size() > 1 && part[0] == '0') break;
-
-            // If this is the first number, start the expression
-            if (i == 0) {
-                help(j + 1, val, val, num, expr + part);
-            } else {
-                // Addition
-                help(j + 1, val, curr + val, num, expr + "+" + part);
-                // Subtraction
-                help(j + 1, -val, curr - val, num, expr + "-" + part);
-                // Multiplication
-                help(j + 1, prev * val, curr - prev + (prev * val), num, expr + "*" + part);
+        for(int j=i;j<num.size();j++){
+            string part=num.substr(i,j-i+1);
+            long val=stol(part);
+            if(part.size()>1 && part[0]=='0' )break;
+            if(i==0){
+                help(j+1,val,val,num ,expr+part);
+            }
+            else{
+                help(j+1,val,curr+val,num,expr+"+"+part);
+                help(j+1,-val,curr-val,num,expr+"-"+part);
+                help(j+1,prev*val,curr-prev+prev*val,num ,expr+"*"+part);
             }
         }
     }
-
     vector<string> addOperators(string num, int target) {
-        this->target = target;
-        if (num.empty()) return ans;
-
-        // Start the recursion
-        help(0, 0, 0, num, "");
+        this->target=target;
+        if(num.empty())return ans;
+        help(0,0,0,num,"");
         return ans;
     }
 };
