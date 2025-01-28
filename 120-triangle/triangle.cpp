7e6f773c0ee02b0adbd1949aba8f96457672788e
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-01-28 19:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (5.78%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -26,37 +26,75 @@ public:
         // return help(0,0,triangle ,dp);
 
         //Tabulation
+        // int n=triangle.size();
+        // vector<vector<int>> dp;
+        // for(int i=0;i<n;i++){
+        //     int m=triangle[i].size();
+        //     dp.push_back(vector<int>(m,1e9));
+        // }
+        // for(int i=0;i<n;i++){
+        //     int m=triangle[i].size();
+        //     for(int j=0;j<m;j++){
+        //         int s=triangle[i][j];
+        //         if(i==0 && j==0){
+        //             dp[i][j]=s;
+        //             continue;
+        //         }
+        //         else{
+        //             int s1=1e9 ,s2=1e9;
+        //             if(j>0){
+        //                 s1=dp[i-1][j-1];
+        //             }
+        //             int m1=triangle[i-1].size();
+        //             if(j<m1)s2=dp[i-1][j];
+        //             dp[i][j]=s+min(s1,s2);
+        //         }
+        //     }
+        // }
+        // int ans=INT_MAX;
+        // int m1=triangle[n-1].size();
+        // for(int i=0;i<m1;i++){
+        //     ans=min(ans,dp[n-1][i]);
+        // }
+        // return ans;
+
+        //space optimization
         int n=triangle.size();
-        vector<vector<int>> dp;
+        vector<int> prev;
         for(int i=0;i<n;i++){
             int m=triangle[i].size();
-            dp.push_back(vector<int>(m,1e9));
-        }
-        for(int i=0;i<n;i++){
-            int m=triangle[i].size();
+            vector<int> curr(m);
             for(int j=0;j<m;j++){
                 int s=triangle[i][j];
                 if(i==0 && j==0){
-                    dp[i][j]=s;
+                    curr[j]=s;
                     continue;
                 }
                 else{
                     int s1=1e9 ,s2=1e9;
                     if(j>0){
-                        s1=dp[i-1][j-1];
+                        s1=prev[j-1];
                     }
                     int m1=triangle[i-1].size();
-                    if(j<m1)s2=dp[i-1][j];
-                    dp[i][j]=s+min(s1,s2);
+                    if(j<m1)s2=prev[j];
+                    curr[j]=s+min(s1,s2);
+
                 }
             }
+            prev.clear();
+            prev.resize(m);
+            prev=curr;
+
         }
         int ans=INT_MAX;
         int m1=triangle[n-1].size();
         for(int i=0;i<m1;i++){
-            ans=min(ans,dp[n-1][i]);
+            ans=min(ans,prev[i]);
         }
         return ans;
+        
+
+
 
     }
 };
