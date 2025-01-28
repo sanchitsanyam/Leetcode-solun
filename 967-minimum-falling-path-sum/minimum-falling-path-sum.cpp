4d8b14828929631bc32cfed1a2d2c3705813a320
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-01-29 03:09:25 +0530

    Time: 2 ms (36.16%) | Memory: 15 MB (39.03%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -26,27 +26,56 @@
 //     }
 // };
 //Tabulation
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int n=matrix.size();
+//         vector<vector<int>>dp(n ,vector<int>(n));
+//         for(int j=0;j<n;j++){
+//             dp[0][j]=matrix[0][j];
+//         }
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 int s=matrix[i][j];
+//                 int s1=1e9 ,s2=1e9,s3=1e9;
+//                 if(i>0)s1=s+dp[i-1][j];
+//                 if(i>0 && j>0)s2=s+dp[i-1][j-1];
+//                 if(i>0 && j+1<n)s3=s+dp[i-1][j+1];
+//                 dp[i][j]=min({s1,s2,s3});
+//             }
+//         }
+//         int ans=INT_MAX;
+//         for(int j=0;j<n;j++){
+//             ans=min(ans,dp[n-1][j]);
+//         }
+//         return ans;
+//     }
+// };
+
+//Space Optimization
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>>dp(n ,vector<int>(n));
+        vector<int>prev(n);
         for(int j=0;j<n;j++){
-            dp[0][j]=matrix[0][j];
+            prev[j]=matrix[0][j];
         }
         for(int i=1;i<n;i++){
+            vector<int> curr(n);
             for(int j=0;j<n;j++){
                 int s=matrix[i][j];
                 int s1=1e9 ,s2=1e9,s3=1e9;
-                if(i>0)s1=s+dp[i-1][j];
-                if(i>0 && j>0)s2=s+dp[i-1][j-1];
-                if(i>0 && j+1<n)s3=s+dp[i-1][j+1];
-                dp[i][j]=min({s1,s2,s3});
+                if(i>0)s1=s+prev[j];
+                if(i>0 && j>0)s2=s+prev[j-1];
+                if(i>0 && j+1<n)s3=s+prev[j+1];
+                curr[j]=min({s1,s2,s3});
             }
+            prev=curr;
         }
         int ans=INT_MAX;
         for(int j=0;j<n;j++){
-            ans=min(ans,dp[n-1][j]);
+            ans=min(ans,prev[j]);
         }
         return ans;
     }
