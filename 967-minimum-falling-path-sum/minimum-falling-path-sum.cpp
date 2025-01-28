4bacfa67ec2b1c0074fffc4e50cb4ac97ba3dfcf
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-01-29 03:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (60.65%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -25,29 +25,29 @@
 //         return ans;
 //     }
 // };
+//Tabulation
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        // Initialize the first row of dp with the first row of matrix
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = matrix[0][j];
+        int n=matrix.size();
+        vector<vector<int>>dp(n ,vector<int>(n));
+        for(int j=0;j<n;j++){
+            dp[0][j]=matrix[0][j];
         }
-        
-        // Fill the dp table
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = matrix[i][j] + min({
-                    dp[i-1][j],
-                    j > 0 ? dp[i-1][j-1] : INT_MAX,
-                    j < n-1 ? dp[i-1][j+1] : INT_MAX
-                });
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int s=matrix[i][j];
+                int s1=1e9 ,s2=1e9,s3=1e9;
+                if(i>0)s1=s+dp[i-1][j];
+                if(i>0 && j>0)s2=s+dp[i-1][j-1];
+                if(i>0 && j+1<n)s3=s+dp[i-1][j+1];
+                dp[i][j]=min({s1,s2,s3});
             }
         }
-        
-        // Find the minimum value in the last row of dp
-        return *min_element(dp[n-1].begin(), dp[n-1].end());
+        int ans=INT_MAX;
+        for(int j=0;j<n;j++){
+            ans=min(ans,dp[n-1][j]);
+        }
+        return ans;
     }
 };
