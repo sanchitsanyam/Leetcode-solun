a722ea459958e7b9fa0affad78fc2b4515658820
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-02-16 19:53:06 +0530

    Time: 6 ms (78.87%) | Memory: 15.2 MB (49.98%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -45,6 +45,31 @@ public:
     // }
 
     //Tabulation
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     int n=nums.size();
+    //     int Sum=accumulate(nums.begin(),nums.end(),0);
+    //     int k=(target+Sum);
+    //     if(abs(target)>Sum)return 0;
+    //     if(k%2!=0)return 0;
+    //     else k=k/2;
+    //     if(k<0)return 0;
+    //     vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+    //     dp[n][k]=1;
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int t=0;t<=k;t++){
+    //             int nt=dp[i+1][t];
+    //             int T=0;
+    //             if(t+nums[i]<=k){
+    //                 T=dp[i+1][t+nums[i]];
+    //             }
+    //             dp[i][t]=nt+T;
+    //         }
+    //     }
+    //     return dp[0][0]; 
+    // }
+    
+
+    /// space optimization
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int Sum=accumulate(nums.begin(),nums.end(),0);
@@ -53,21 +78,22 @@ public:
         if(k%2!=0)return 0;
         else k=k/2;
         if(k<0)return 0;
-        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
-        dp[n][k]=1;
+        vector<int> prev(k+1,0);
+        prev[k]=1;
         for(int i=n-1;i>=0;i--){
+            vector<int> curr(k+1,0);
             for(int t=0;t<=k;t++){
-                int nt=dp[i+1][t];
+                int nt=prev[t];
                 int T=0;
                 if(t+nums[i]<=k){
-                    T=dp[i+1][t+nums[i]];
+                    T=prev[t+nums[i]];
                 }
-                dp[i][t]=nt+T;
+                curr[t]=nt+T;
             }
+            prev=curr;
         }
-        return dp[0][0]; 
+        return prev[0]; 
     }
-    
     
     
 };
