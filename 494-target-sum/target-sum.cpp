a43b51aea141bcce166719050961551ed1ff5557
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-02-16 19:49:35 +0530

    Time: 4 ms (83.47%) | Memory: 15.7 MB (40.47%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -19,19 +19,32 @@ public:
     // }
 
 
-    // recursion another Way
-    int help(int i, int Csum , vector<int>& nums, int target,vector<vector<int>>& dp){
-        int n=nums.size();
-        if(Csum>target)return 0;
-        if(i==n){
-            if(Csum==target)return 1;
-            else return 0;
-        }
-        if(dp[i][Csum]!=-1)return dp[i][Csum];
-        int  nT=help(i+1,Csum,nums,target,dp);
-        int  T=help(i+1,Csum+nums[i],nums,target,dp);
-        return dp[i][Csum]=nT+T;
-    }
+    // recursion + Memoization another Way
+    // int help(int i, int Csum , vector<int>& nums, int target,vector<vector<int>>& dp){
+    //     int n=nums.size();
+    //     if(Csum>target)return 0;
+    //     if(i==n){
+    //         if(Csum==target)return 1;
+    //         else return 0;
+    //     }
+    //     if(dp[i][Csum]!=-1)return dp[i][Csum];
+    //     int  nT=help(i+1,Csum,nums,target,dp);
+    //     int  T=help(i+1,Csum+nums[i],nums,target,dp);
+    //     return dp[i][Csum]=nT+T;
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     int n=nums.size();
+    //     int Sum=accumulate(nums.begin(),nums.end(),0);
+    //     int k=(target+Sum);
+    //     if(abs(target)>Sum)return 0;
+    //     if(k%2!=0)return 0;
+    //     else k=k/2;
+    //     if(k<0)return 0;
+    //     vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    //     return help(0,0,nums,k,dp);   
+    // }
+
+    //Tabulation
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
         int Sum=accumulate(nums.begin(),nums.end(),0);
@@ -40,9 +53,21 @@ public:
         if(k%2!=0)return 0;
         else k=k/2;
         if(k<0)return 0;
-        vector<vector<int>> dp(n,vector<int>(k+1,-1));
-        return help(0,0,nums,k,dp);
-        
+        vector<vector<int>> dp(n+1,vector<int>(k+1,0));
+        dp[n][k]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int t=0;t<=k;t++){
+                int nt=dp[i+1][t];
+                int T=0;
+                if(t+nums[i]<=k){
+                    T=dp[i+1][t+nums[i]];
+                }
+                dp[i][t]=nt+T;
+            }
+        }
+        return dp[0][0]; 
     }
+    
+    
     
 };
