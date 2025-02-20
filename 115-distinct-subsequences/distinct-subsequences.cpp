8a2b2ce5f7c973946e9ab17ef4e1122cbf614087
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-02-21 02:55:21 +0530

    Time: 29 ms (63.75%) | Memory: 43.5 MB (45.39%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -25,27 +25,56 @@ public:
     // }
 
     //Tabulation
+    // int numDistinct(string s, string t) {
+    //     int n=s.size();
+    //     int m=t.size();
+    //     vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
+    //     for(int i=0;i<=n;i++){
+    //         dp[i][m]=1;
+    //     }
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=m-1;j>=0;j--){
+    //             unsigned long long ans1=0 ,ans2=0,ans3=0;
+    //             if(s[i]==t[j]){
+    //                 ans1=(i+1<=n && j+1<=m)?dp[i+1][j+1]:0;
+    //                 ans2=(i+1<=n)?dp[i+1][j]:0;
+    //             }
+    //             else{
+    //                 ans3=(i+1<=n)?dp[i+1][j]:0;
+    //             }
+    //             dp[i][j]=(ans1+ans2+ans3);
+    //         }
+    //     }
+    //     return dp[0][0];
+        
+    // }
+
+
+    //space optimization
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
+       vector<unsigned long long> prev(m+1,0);
         for(int i=0;i<=n;i++){
-            dp[i][m]=1;
+            prev[m]=1;
         }
         for(int i=n-1;i>=0;i--){
+            vector<unsigned long long> curr(m+1,0); 
+            curr[m]=1;
             for(int j=m-1;j>=0;j--){
                 unsigned long long ans1=0 ,ans2=0,ans3=0;
                 if(s[i]==t[j]){
-                    ans1=(i+1<=n && j+1<=m)?dp[i+1][j+1]:0;
-                    ans2=(i+1<=n)?dp[i+1][j]:0;
+                    ans1=(i+1<=n && j+1<=m)?prev[j+1]:0;
+                    ans2=(i+1<=n)?prev[j]:0;
                 }
                 else{
-                    ans3=(i+1<=n)?dp[i+1][j]:0;
+                    ans3=(i+1<=n)?prev[j]:0;
                 }
-                dp[i][j]=(ans1+ans2+ans3);
+                curr[j]=(ans1+ans2+ans3);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
         
     }
 };
