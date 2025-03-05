28e71202659bdc283f0d0fe1a8ef3e2867a39957
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-03-06 03:04:56 +0530

    Time: 35 ms (44.13%) | Memory: 14.9 MB (51.79%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
-    bool  help(int i, int j, string s, string p ,vector<vector<int>>& dp){
-        if(j==p.size()) return i==s.size();
-        if(dp[i][j]!=-1)return dp[i][j];
-        bool first_match=(i<s.size() && (s[i]==p[j] || p[j]=='?'));
-        if(p[j]=='*'){
-            return dp[i][j]=help(i,j+1,s,p,dp)||(i<s.size() && help(i+1,j,s,p,dp));
-        }
-        return dp[i][j]=first_match && help(i+1,j+1 ,s,p,dp);
-    }
+    //Recursion + Memoization
+    // bool  help(int i, int j, string s, string p ,vector<vector<int>>& dp){
+    //     if(j==p.size()) return i==s.size();
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     bool first_match=(i<s.size() && (s[i]==p[j] || p[j]=='?'));
+    //     if(p[j]=='*'){
+    //         return dp[i][j]=help(i,j+1,s,p,dp)||(i<s.size() && help(i+1,j,s,p,dp));
+    //     }
+    //     return dp[i][j]=first_match && help(i+1,j+1 ,s,p,dp);
+    // }
+    // bool isMatch(string s, string p) {
+    //     int m=s.size(),n=p.size();
+    //     vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+    //     return help(0,0,s,p,dp);
+    // }
+
+    // Tabulation
     bool isMatch(string s, string p) {
         int m=s.size(),n=p.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return help(0,0,s,p,dp);
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        dp[m][n]=1;
+        for(int j=n-1;j>=0;j--){
+            if(p[j]!='*')break;
+            dp[m][j]=true;
+        }
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                bool first_match=(s[i]==p[j] || p[j]=='?');
+                if(p[j]=='*'){
+                    dp[i][j]= dp[i][j+1] || dp[i+1][j];
+                }
+                else dp[i][j]= first_match && dp[i+1][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
