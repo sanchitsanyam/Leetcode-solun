2383b62d28953b12e32290a3de9281f2efd04e0d
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-03-06 08:40:15 +0530

    Time: 1 ms (59.50%) | Memory: 16.3 MB (53.74%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,50 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> ans;
-    map<int, map<int, multiset<int>>> nodes;
-    int minCol = 0, maxCol = 0;
-
-    int diameter(TreeNode* root, int& height) {
-        if (root == nullptr) {
-            height = 0;
-            return 0;
-        }
-
-        int lh = 0, rh = 0;
-        int ld = diameter(root->left, lh);
-        int rd = diameter(root->right, rh);
-
-        height = max(lh, rh) + 1;
-        return max({ld, rd, lh + rh + 1});
-    }
-
-    void dfs(TreeNode* root, int row, int col) {
-        if (root == nullptr) return;
-
-        minCol = min(minCol, col);
-        maxCol = max(maxCol, col);
-
-        nodes[col][row].insert(root->val);
-
-        dfs(root->left, row + 1, col - 1);
-        dfs(root->right, row + 1, col + 1);
-    }
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        int height = 0;
-        int diam = diameter(root, height);
-        cout << "Diameter: " << diam << endl;
-
-        dfs(root, 0, 0);
-
-        for (int col = minCol; col <= maxCol; col++) {
-            vector<int> colNodes;
-            for (auto& [row, values] : nodes[col]) {
-                colNodes.insert(colNodes.end(), values.begin(), values.end());
+        map<int,map<int,multiset<int>>> nodes;
+        queue<pair<TreeNode* ,pair<int,int>>> todo;
+        todo.push({root,{0,0}});
+        while(!todo.empty()){
+            auto p=todo.front();
+            todo.pop();
+            TreeNode* curr=p.first;
+            int x=p.second.first,y=p.second.second;
+            nodes[x][y].insert(curr->val);
+            if(curr->left){
+                todo.push({curr->left,{x-1,y+1}});
             }
-            ans.push_back(colNodes);
-        }
+            if(curr->right){
+                todo.push({curr->right,{x+1,y+1}});
+            }
 
+        }
+        vector<vector<int>>ans;
+        for(auto p:nodes){
+            vector<int> col;
+            for(auto q:p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
+            }
+            ans.push_back(col);
+        }
         return ans;
     }
 };
