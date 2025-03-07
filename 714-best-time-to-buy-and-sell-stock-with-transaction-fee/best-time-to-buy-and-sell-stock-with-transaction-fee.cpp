fa14569bc3f2fe12ef06c6a36bbc31b54193bc28
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-03-08 03:37:23 +0530

    Time: 103 ms (37.39%) | Memory: 94.9 MB (54.89%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,46 @@
 class Solution {
 public:
-    int help(int i ,int buy, int f,vector<int>& prices,vector<vector<int>>& dp){
-        if(i==prices.size())return 0;
-        int opn1,opn2;
-        if(dp[i][buy]!=-1)return dp[i][buy];
-        if(buy==0){
-            opn1=-prices[i]-f+help(i+1,1,f,prices,dp);
-            opn2=help(i+1,0,f,prices,dp);
-        }
-        else{
-            opn1=prices[i]+help(i+1,0,f,prices,dp);
-            opn2=help(i+1,1,f,prices,dp);
-        }
-        return dp[i][buy]=max(opn1,opn2);
-    }
-    int maxProfit(vector<int>& prices, int fee) {
+    //Recursive +Memoization
+    // int help(int i ,int buy, int f,vector<int>& prices,vector<vector<int>>& dp){
+    //     if(i==prices.size())return 0;
+    //     int opn1,opn2;
+    //     if(dp[i][buy]!=-1)return dp[i][buy];
+    //     if(buy==0){
+    //         opn1=-prices[i]-f+help(i+1,1,f,prices,dp);
+    //         opn2=help(i+1,0,f,prices,dp);
+    //     }
+    //     else{
+    //         opn1=prices[i]+help(i+1,0,f,prices,dp);
+    //         opn2=help(i+1,1,f,prices,dp);
+    //     }
+    //     return dp[i][buy]=max(opn1,opn2);
+    // }
+    // int maxProfit(vector<int>& prices, int fee) {
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n,vector<int>(2,-1));
+    //     return help(0,0,fee,prices,dp);
+    // }
+
+    //Tabulation
+     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return help(0,0,fee,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        dp[n][0]=0, dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                int opn1 ,opn2;
+                if(j==0){
+                    opn1=-prices[i]-fee+dp[i+1][1];
+                    opn2=dp[i+1][0];
+                }
+                else{
+                    opn1=prices[i]+dp[i+1][0];
+                    opn2=dp[i+1][1];
+                }
+                dp[i][j]=max(opn1 ,opn2);
+            }
+        }
+        return dp[0][0];
+    
     }
 };
