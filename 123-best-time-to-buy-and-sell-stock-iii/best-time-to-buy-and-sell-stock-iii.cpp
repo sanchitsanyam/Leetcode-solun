a388f7e75c8807ae10eb8820d70653bfef4034b6
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-03-07 14:08:17 +0530

    Time: 575 ms (5.01%) | Memory: 258.5 MB (5.42%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -23,36 +23,71 @@ public:
     // }
 
     //Tabulation
-     int maxProfit(vector<int>& prices) {
+    //  int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(5,0)));
+    //     for(int j=0;j<=1;j++){
+    //         for(int k=0;k<5;k++){
+    //             dp[n][j][k]=0;
+    //         }
+    //     }
+    //     for(int i=0;i<=n;i++){
+    //         for(int j=0;j<=1;j++){
+    //             dp[i][j][0]=0;
+    //         }
+    //     }
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=0;j<=1;j++){
+    //             for(int k=1;k<5;k++){
+    //                 int opn1,opn2;
+    //                 if(j==0){
+    //                     opn1=-prices[i]+dp[i+1][1][k-1];
+    //                     opn2=dp[i+1][j][k];
+    //                 }
+    //                 else{
+    //                     opn1=prices[i]+dp[i+1][0][k-1];
+    //                     opn2=dp[i+1][j][k];
+    //                 }
+    //                 dp[i][j][k]=max(opn1,opn2);
+    //             }
+    //         }
+    //     }
+    //     return dp[0][0][4];
+    // }
+
+    // Space optimiztaion 
+    int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(5,0)));
+        vector<vector<int>> prev(2,vector<int>(5,0));
         for(int j=0;j<=1;j++){
             for(int k=0;k<5;k++){
-                dp[n][j][k]=0;
+                prev[j][k]=0;
             }
         }
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=1;j++){
-                dp[i][j][0]=0;
-            }
+        for(int j=0;j<=1;j++){
+            prev[j][0]=0;
         }
         for(int i=n-1;i>=0;i--){
+            vector<vector<int>> curr(2,vector<int>(5,0));
             for(int j=0;j<=1;j++){
                 for(int k=1;k<5;k++){
                     int opn1,opn2;
                     if(j==0){
-                        opn1=-prices[i]+dp[i+1][1][k-1];
-                        opn2=dp[i+1][j][k];
+                        opn1=-prices[i]+prev[1][k-1];
+                        opn2=prev[j][k];
                     }
                     else{
-                        opn1=prices[i]+dp[i+1][0][k-1];
-                        opn2=dp[i+1][j][k];
+                        opn1=prices[i]+prev[0][k-1];
+                        opn2=prev[j][k];
                     }
-                    dp[i][j][k]=max(opn1,opn2);
+                    curr[j][k]=max(opn1,opn2);
                 }
+                
             }
+            prev=curr;
         }
-        return dp[0][0][4];
+        return prev[0][4];
     }
+
 
 };
