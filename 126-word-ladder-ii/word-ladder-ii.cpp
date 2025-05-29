07adc6d27bd97f6a19e702f641375b68237869cf
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-05-29 17:45:36 +0530

    Time: 5 ms (98.35%) | Memory: 13.7 MB (40.22%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.cpp
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -1,55 +1,50 @@
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> dict(wordList.begin(), wordList.end());
-        if (!dict.count(endWord)) return {};
-
-        unordered_map<string, vector<string>> parents; // child -> list of parents
-        unordered_set<string> currentLevel{beginWord};
-        bool found = false;
-
-        while (!currentLevel.empty() && !found) {
-            unordered_set<string> nextLevel;
-            for (const string& word : currentLevel)
-                dict.erase(word);  // prevent revisiting
-
-            for (const string& word : currentLevel) {
-                string temp = word;
-                for (int i = 0; i < temp.size(); ++i) {
-                    char original = temp[i];
-                    for (char c = 'a'; c <= 'z'; ++c) {
-                        temp[i] = c;
-                        if (dict.count(temp)) {
-                            parents[temp].push_back(word);
-                            nextLevel.insert(temp);
-                            if (temp == endWord) found = true;
+        unordered_set<string> List(wordList.begin(),wordList.end());
+        if(List.find(endWord)==List.end())return{};
+        unordered_map<string,vector<string>> p;
+        unordered_set<string> Clevel{beginWord};//Currentlevel
+        bool f=false;//found;
+        while(!Clevel.empty() &&  !f){
+            unordered_set<string> Nlevel;//NextLevel
+            for(auto& s:Clevel){
+                List.erase(s);
+            }
+            for(auto& temp:Clevel){
+                string k=temp;
+                for(int i=0;i<k.size();i++){
+                    char original=k[i];
+                    for(char c='a';c<='z';c++){
+                        k[i]=c;
+                        if(List.find(k)!=List.end()){
+                            Nlevel.insert(k);
+                            p[k].push_back(temp);
+                            if(k==endWord)f=true;
                         }
                     }
-                    temp[i] = original;
-                }
+                    k[i]=original;
+                }    
             }
-
-            currentLevel = nextLevel;
+            Clevel=Nlevel;
         }
-
         vector<vector<string>> res;
         vector<string> path{endWord};
-
-        function<void(string)> backtrack = [&](string word) {
-            if (word == beginWord) {
-                vector<string> temp = path;
-                reverse(temp.begin(), temp.end());
-                res.push_back(temp);
-                return;
+        function<void(string)> backtrack=[&](string word){
+            if(word==beginWord){
+               vector<string> temp=path;
+               reverse(temp.begin(),temp.end());
+               res.push_back(temp);
+               return;
             }
-            for (const string& par : parents[word]) {
+            for(auto& par:p[word]){
                 path.push_back(par);
                 backtrack(par);
                 path.pop_back();
             }
         };
 
-        if (found) backtrack(endWord);
-        return res;
+        if(f)backtrack(endWord);
+        return  res;
     }
 };
