d30cd08c5c67103b964cf1b8c6e0d10a1e0bbcbd
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-09-17 03:52:06 +0530

    Time: 239 ms (5.07%) | Memory: 178.1 MB (5.07%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void check(stack<int>& s){
+        if(s.size()<2)return;
+        int x=s.top();
+        s.pop();
+        int y=s.top();
+        if(gcd(x,y)!=1){
+            s.pop();
+            s.push(lcm(x,y));
+            if(s.size()>=2)check(s);
+        }
+        else{
+            s.push(x);
+            
+        }    
+    }
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        stack<int> s;
+        int n=nums.size();
+        s.push(nums[0]);
+        for(int i=1; i<n;i++){
+            
+            int y=s.top();
+            if(gcd(nums[i],y)!=1){
+                s.pop();
+                s.push(lcm(nums[i],y));
+                check(s);
+                
+            }
+            else{
+                s.push(nums[i]);
+                check(s);
+            }
+        }
+
+
+        vector<int> ans;
+        int m=s.size();
+        for(int i=0 ; i<m ; i++){
+            int x=s.top();
+            s.pop();
+            
+            ans.push_back(x);
+        }
+        cout<<gcd(2009,899)<<" "<<gcd(899,20677);
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
