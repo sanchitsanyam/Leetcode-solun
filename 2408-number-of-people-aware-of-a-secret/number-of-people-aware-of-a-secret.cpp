bd8d0a9a1de9193daf2a10ae72b309becae20259
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-09-17 16:45:49 +0530

    Time: 15 ms (23.37%) | Memory: 9.3 MB (90.05%) - LeetSync

---
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int help(int start ,int delay, int forget,int n,vector<int>& dp){
+        if(delay==forget)return 1;
+        if(dp[start]!=-1)return dp[start];
+        int m=(start+forget>n)?1:0;
+        int ans=1*m;
+        for(int i=start+delay;i<min(start+forget,n+1);i++){
+            ans=(ans+help(i,delay,forget,n,dp))%(mod);
+        }
+        return dp[start]=ans;
+    }
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> dp(n+1,-1);
+        return help(1,delay,forget,n,dp);
+    }
+};
