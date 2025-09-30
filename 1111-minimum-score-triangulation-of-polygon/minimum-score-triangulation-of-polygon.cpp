9ffbcb0e80888c1d1ab28f59e0b420a0694de02f
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-10-01 00:38:25 +0530

    Time: 1 ms (72.96%) | Memory: 11.2 MB (70.90%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -1,20 +1,41 @@
+// class Solution {
+// public:
+//     int solve(int i,int j,vector<int>& values, vector<vector<int>>& dp){
+//         if((j-i)<2)return 0;
+//         if(dp[i][j]!=-1)return dp[i][j];
+//         int fans=INT_MAX;
+//         for(int k=i+1;k<j;k++){
+//             int left=solve(i,k,values,dp);
+//             int right=solve(k,j,values,dp);
+//             int ans=left+right+1LL*values[i]*values[k]*values[j];
+//             fans=min(fans,ans);
+//         }
+//         return dp[i][j]=fans;
+//     }
+//     int minScoreTriangulation(vector<int>& values) {
+//         int n=values.size();
+//         vector<vector<int>> dp(n,vector<int>(n,-1));
+//         return solve(0,n-1,values,dp);
+//     }
+// };
 class Solution {
 public:
-    int solve(int i,int j,vector<int>& values, vector<vector<int>>& dp){
-        if((j-i)<2)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int fans=INT_MAX;
-        for(int k=i+1;k<j;k++){
-            int left=solve(i,k,values,dp);
-            int right=solve(k,j,values,dp);
-            int ans=left+right+1LL*values[i]*values[k]*values[j];
-            fans=min(fans,ans);
-        }
-        return dp[i][j]=fans;
-    }
+    
     int minScoreTriangulation(vector<int>& values) {
         int n=values.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solve(0,n-1,values,dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+2;j<n;j++){
+                int x=INT_MAX;
+                for(int k=i+1;k<j;k++){
+                    int left=dp[i][k];
+                    int right=dp[k][j];
+                    int r=left+right+1LL*values[i]*values[k]*values[j];
+                    x=min(x,r);
+                }
+                dp[i][j]=x;
+            }
+        }
+        return dp[0][n-1];
     }
 };
