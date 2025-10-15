bab994212a7f33e3aa7defcbdd7fd0fa8e840f82
SANCHIT SANYAM <111212970+sanchitsanyam@users.noreply.github.com>
2025-10-15 11:27:35 +0530

    Time: 16 ms (42.73%) | Memory: 40.5 MB (54.55%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+    bool hasIncreasingSubarrays(vector<int>& a, int k) {
         if(k==1)return true;
-        int n=nums.size();
-        vector<int> arr(n-1,0);
-        int c=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(c+1==2*k)return true;
-            if(nums[i]<nums[i+1])c++;
-            else c=0;
-        }
-        if(c+1==2*k)return true;
-        for(int i=0;i<n-1;i++){
-            arr[i]=nums[i+1]-nums[i];
-        }
-        vector<int> s;
-        int t=0;
-        for(int i=0;i<n-1;i++){
-            if(arr[i]>0)t++;
-            else  {
-                s.push_back(t);
-                t=0;
+        int n=a.size();
+        int prev=0;
+        int curr=1;
+        for(int i=1;i<n;i++){
+            if(a[i]>a[i-1]){
+               curr++;
+            }
+            else{
+                if(prev>=k && curr>=k)return true;
+                else if (curr>=2*k)return true;
+                else{
+                    prev=curr;
+                    curr=1;
+                }
             }
         }
-        s.push_back(t);
-        int d=0;
-        for(int i=0;i<s.size()-1;i++){
-            
-            if(s[i]>=k-1 && s[i+1]>=k-1)return true;
-        }
+        if(prev>=k && curr>=k)return true;
+        else if (curr>=2*k)return true;
         return false;
     }
 };
